bf396c3f56558f2ed65fb0fc705660f963781178
wellingtondellamura <della.mura@gmail.com>
2018-05-02 19:40:34 -0300

    ddd

--- mini-projects/mini-twitter/aux/mini-twitter.sql
+++ mini-projects/mini-twitter/aux/mini-twitter.sql
@@ -1,14 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 02, 2018 at 07:39 PM
+-- Server version: 10.0.34-MariaDB-0ubuntu0.16.04.1
+-- PHP Version: 7.0.28-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mini-twitter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `twitts`
+--
 
 CREATE TABLE `twitts` (
   `id` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `owner` varchar(20) NOT NULL,
-  `message` varchar(140) NOT NULL
+  `message` varchar(140) NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
 
-ALTER TABLE `twitts`
-  ADD PRIMARY KEY (`id`);
+--
+-- Table structure for table `users`
+--
 
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(500) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `removed` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `date`, `removed`) VALUES
+(1, 'Wellington Della Mura', 'TheMasterOfTwitts', 'della.mura@gmail.com', '1234', '2018-05-02 19:51:53', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `twitts`
+--
 ALTER TABLE `twitts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_twitts_users_idx` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email_UNIQUE` (`email`),
+  ADD UNIQUE KEY `username_UNIQUE` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `twitts`
+--
+ALTER TABLE `twitts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `twitts`
+--
+ALTER TABLE `twitts`
+  ADD CONSTRAINT `fk_twitts_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
